b1091c35a995085f090cd8d064766c9c7a2049c1
yoji kuretake <yojikuretake@yojinoMacBook-Air.local>
2021-04-13 03:22:59 +0900

    commitしたよ

---
+++ sample.py
@@ -0,0 +1,12 @@
+class  math_many:
+    def squre(x):
+        return x*x
+
+    def sum_x(x):
+        return sum(x)
+
+
+if __name__ == '__main__':
+    v = [1,5,7,9,4]
+    obj = class()
+    print(obj.squre(x)) 
